26f45facbc4f8dc4ba5c8177b76b4aa2b809b49d
Erwin Kroon <123574+ekroon@users.noreply.github.com>
2022-12-06 08:27:18 +0100

    Day06, with window

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,29 +1,29 @@
 use std::collections::HashSet;
 
-fn find_network_start_marker(input: &str, n: usize) -> (String, usize) {
+fn find_network_start_marker(input: &str, n: usize) -> usize {
     // loop over the input string, n characters at a time, count the number of different characters
-    // in the substring, and return the string and current index + n into the string
-    for (i, c) in input.chars().enumerate() {
-        let mut set = HashSet::new();
-        set.insert(c);
-        for j in 1..n {
-            if let Some(c) = input.chars().nth(i + j) {
-                set.insert(c);
-            }
-        }
-        if set.len() == n {
-            return (input[i..i + n].to_string(), i + n);
-        }
-    }
-    ("".to_string(), 0)
+    // in the substring, and return the current index + n into the string.
+    input
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(n)
+        .enumerate()
+        .find(|(_, window)| {
+            window
+                .iter()
+                .collect::<HashSet<&char>>()
+                .len() == window.len()
+        })
+        .map(|(i, _)| i + n)
+        .unwrap()
 }
 
 fn run() -> (usize, usize) {
     let input = include_str!("input.txt");
 
     (
-        find_network_start_marker(input, 4).1,
-        find_network_start_marker(input, 14).1,
+        find_network_start_marker(input, 4),
+        find_network_start_marker(input, 14),
     )
 }
 
