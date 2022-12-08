6dc10d151ae2947f87f7f54ab45f6e38de4cda47
Erwin Kroon <123574+ekroon@users.noreply.github.com>
2022-12-08 09:16:34 +0100

    Day08: part2
    Copilot in general is great at generating code that has off by one errors.
    This makes it necessary to validate all the generated code manual ...

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -17,7 +17,7 @@ fn parse(input: &str) -> HashMap<(usize, usize), usize> {
 }
 
 // "solve" takes the parsed input from "parse" and in all directions (up, down, left, right) finds all the numbers that are higher than the previous number and counts them
-fn solve(input: HashMap<(usize, usize), usize>) -> HashSet<(usize, usize)> {
+fn solve(input: HashMap<(usize, usize), usize>) -> HashSet<(usize, usize)>  {
     // consider the input representing a grid of numbers of size sqrt(input.len())
     // line by line, remember the highest number seen in the row and if the current number is higher, add the key to a set
     // Consider 0 as valid input so use an Option<usize> to represent the highest number seen so far
@@ -68,8 +68,65 @@ fn solve(input: HashMap<(usize, usize), usize>) -> HashSet<(usize, usize)> {
     valid_keys
 }
 
+// function "solve2" finds the visible numbers from every position in the grid in all the directions (up, down, left, right) and returns the highest multiple of those numbers
+fn solve2(input: HashMap<(usize, usize), usize>) -> usize {
+    let grid_size = (input.len() as f64).sqrt() as usize;
+    let mut highest_multiple = 0;
+    for line in 0..(input.len() as f64).sqrt() as usize {
+        for pos in 0..(input.len() as f64).sqrt() as usize {
+            let current_number = input.get(&(line, pos)).unwrap();
+            // count the numbers left of the current number which are lower than the current number
+            let mut count_left = 0;
+            for i in 0..pos {
+                if input.get(&(line, pos - i - 1)).unwrap() < current_number {
+                    count_left += 1;
+                } else {
+                    count_left += 1;
+                    break;
+                }
+            }
+            // count the numbers right of the current number which are lower than the current number
+            let mut count_right = 0;
+            for i in 0..(grid_size - pos - 1) {
+                if input.get(&(line, pos + i + 1)).unwrap() < current_number {
+                    count_right += 1;
+                } else {
+                    count_right += 1;
+                    break;
+                }
+            }
+            // count the numbers above the current number which are lower than the current number
+            let mut count_top = 0;
+            for i in 0..line {
+                if input.get(&(line - i - 1, pos)).unwrap() < current_number {
+                    count_top += 1;
+                } else {
+                    count_top += 1;
+                    break;
+                }
+            }
+            // count the numbers below the current number which are lower than the current number
+            let mut count_bottom = 0;
+            for i in 0..(grid_size - line - 1) {
+                if input.get(&(line + i + 1, pos)).unwrap() < current_number {
+                    count_bottom += 1;
+                } else {
+                    count_bottom += 1;
+                    break;
+                }
+            }
+            let multiple = count_left * count_right * count_top * count_bottom;
+            if multiple > highest_multiple {
+                highest_multiple = multiple;
+            }
+        }
+    }
+    highest_multiple
+}
+
 fn main() {
     println!("Part 1: {}", solve(parse(INPUT)).len());
+    println!("Part 2: {}", solve2(parse(INPUT)));
 }
 
 #[cfg(test)]
@@ -99,5 +156,10 @@ mod tests {
         for (line, pos) in expected.iter() {
             assert_eq!(result.contains(&(*line, *pos)), true, "expected {:?} to be in result", (*line, *pos));
         }
+    }
+
+    #[test]
+    fn test_total2() {
+        assert_eq!(solve2(parse(TEST_INPUT)), 8);
     }
 }
