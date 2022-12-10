822d05f34eb3bbad187eb013853a3c0476a5b953
Erwin Kroon <123574+ekroon@users.noreply.github.com>
2022-12-10 10:28:54 +0100

    Day10

---
+++ day10/src/main.rs
@@ -0,0 +1,93 @@
+use itertools::Itertools;
+
+const INPUT: &str = include_str!("input.txt");
+const CYCLES: [usize; 6] = [20, 60, 100, 140, 180, 220];
+
+enum Instruction {
+    Noop,
+    AddX(i64)
+}
+
+fn cycle_values(input: &str) -> impl Iterator<Item = (usize, i64)> + '_ {
+    let mut x = 1i64;
+    let mut cycle = 0usize;
+    input.lines().map(|line| {
+        let mut parts = line.split_whitespace();
+        match parts.next().unwrap() {
+            "noop" => Instruction::Noop,
+            "addx" => Instruction::AddX(parts.next().unwrap().parse().unwrap()),
+            _ => panic!("invalid instruction")
+        }
+    }).flat_map(move |instruction| {
+        match instruction {
+            Instruction::Noop => {
+                cycle += 1;
+                vec![(cycle, x)]
+            },
+            Instruction::AddX(value) => {
+                let result = vec![(cycle + 1, x), (cycle + 2, x)];
+                x += value;
+                cycle += 2;
+                result
+            }
+        }
+    })
+}
+
+fn part1(input: &str, cycles: &[usize]) -> Vec<i64> {
+    cycle_values(input).fold(vec![], |mut v, (cycle, value)| {
+        if cycles.contains(&cycle) {
+            v.push(cycle as i64 * value);
+        }
+        v
+    })
+}
+
+fn part2(input: &str) -> Vec<String> {
+    let chars = cycle_values(input).fold(vec![], |mut v, (cycle, value)| {
+        let column = ((cycle - 1) % 40) as i64;
+        if column >= (value - 1) && column <= (value + 1) {
+            v.push('#')
+        } else {
+            v.push('.')
+        }
+        v
+    });
+
+    let mut result = vec![];
+    for chunk in &chars.into_iter().chunks(40) {
+        result.push(chunk.collect());
+    }
+    result
+}
+
+fn main() {
+    let part1 = part1(INPUT, &CYCLES).into_iter().sum::<i64>();
+    println!("Part 1: {part1}");
+    println!("Part 2: ");
+    let part2 = part2(INPUT);
+    for line in part2 {
+        println!("{line}");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use test_case::test_case;
+    use super::*;
+
+    const SAMPLE_INPUT: &str = include_str!("sample.txt");
+
+    #[test_case(SAMPLE_INPUT, &[20] => 420)]
+    #[test_case(SAMPLE_INPUT, &CYCLES => 13140)]
+    #[test_case(INPUT, &CYCLES => 15020)]
+    fn test_part1(input: &str, cycles: &[usize]) -> i64 {
+        part1(input, cycles).into_iter().sum()
+    }
+
+    #[test_case(SAMPLE_INPUT)]
+    #[test_case(INPUT)]
+    fn test_part2(input: &str) {
+        insta::assert_debug_snapshot!(part2(input));
+    }
+}
