c0c2656aab613dcadc36f09cab828c2f30f7f6a0
Erwin Kroon <123574+ekroon@users.noreply.github.com>
2022-12-13 08:35:30 +0100

    Day12: optimized

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,71 +1,67 @@
-use std::collections::HashSet;
 use petgraph::algo::dijkstra;
 use petgraph::Graph;
+use std::collections::{HashMap, HashSet};
 
 const INPUT: &str = include_str!("input.txt");
 
 fn solve(input: &str) -> (i32, i32) {
-
-    let mut points = Vec::new();
-
-    let mut start = None;
-    let mut end = None;
-    for (i, line) in input.lines().enumerate() {
-        for (j, ch) in line.chars().enumerate() {
-            if ch == 'S' {
-                points.push((i, j, 'a'));
-                start.replace((i,j,'a'));
-            }
-            if ch == 'E' {
-                points.push((i, j, 'z'));
-                end.replace((i,j,'z'));
-            } else {
-                points.push((i, j, ch));
-            }
-        }
-    }
-
+    let mut points = HashMap::new();
     let mut graph = Graph::new();
     let mut a_nodes = HashSet::new();
+    let mut start = None;
+    let mut end = None;
 
-    for &(i, j, ch) in points.iter() {
-        let idx = graph.add_node((i, j, ch));
-        if ch == 'a' {
-            a_nodes.insert(idx);
+    for (i, line) in input.lines().enumerate() {
+        for (j, ch) in line.chars().enumerate() {
+            let mut is_start = false;
+            let mut is_end = false;
+            let mut ch = ch;
+            if ch == 'S' {
+                is_start = true;
+                ch = 'a';
+            } else if ch == 'E' {
+                is_end = true;
+                ch = 'z';
+            }
+            let idx= graph.add_node(());
+            if is_start {
+                start.replace(idx);
+            }
+            if is_end {
+                end.replace(idx);
+            }
+            if ch == 'a' {
+                a_nodes.insert(idx);
+            }
+            points.insert((i,j), (ch, idx));
         }
     }
 
-    for &(i, j, ch) in points.iter() {
-        for &(k, l, ch2) in points.iter() {
-            if (i, j) == (k, l) {
-                continue;
-            }
+    for (&(i, j), &(ch, node1)) in points.iter() {
+        let neighbours = [
+            (i.wrapping_sub(1), j),
+            (i.wrapping_add(1), j),
+            (i, j.wrapping_sub(1)),
+            (i, j.wrapping_add(1)),
+        ];
 
-            // compare if ch is one lower than c2
-
-            if ch as u8 + 1 >= ch2 as u8 {
-                // Check if (k, l) is a neighbor of (i, j)
-                if  (i, j+1) == (k, l) ||
-                    (j > 0) && (i, j-1) == (k, l) ||
-                    (i+1, j) == (k, l) ||
-                    (i > 0) && (i-1, j) == (k, l) {
-                    // get node (i, j) from graph
-                    let node1 = graph.node_indices().find(|&n| graph[n] == (i, j, ch)).unwrap();
-                    let node2 = graph.node_indices().find(|&n| graph[n] == (k, l, ch2)).unwrap();
-                    graph.add_edge(node1, node2, 1);
+        for (k, l) in neighbours {
+            if let Some(&(ch2, node2)) = points.get(&(k, l)) {
+                if ch as u8 + 1 >= ch2 as u8 {
+                    graph.add_edge(node2, node1, 1);
                 }
             }
         }
     }
 
-    let start = graph.node_indices().find(|&n| graph[n] == start.unwrap());
-    let end = graph.node_indices().find(|&n| graph[n] == end.unwrap());
+    let results = dijkstra(&graph, end.unwrap(), None, |_| 1);
 
-    let part1 = *dijkstra(&graph, start.unwrap(), end, |_| 1).get(&end.unwrap()).unwrap();
-
-    let part2 = a_nodes.iter().filter_map(|&idx| {
-        dijkstra(&graph, idx, end, |_| 1).get(&end.unwrap()).copied()
-    }).min().unwrap();
+    let part1 = *results.get(&start.unwrap()).unwrap();
+    let part2 = *a_nodes
+        .iter()
+        .filter_map(|idx| results.get(idx))
+        .min()
+        .unwrap();
     (part1, part2)
 }
 
@@ -77,8 +73,8 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use test_case::test_case;
     use super::*;
+    use test_case::test_case;
 
     const SAMPLE: &str = include_str!("sample.txt");
 
